8c143ab2468e053c06d1bb50c20556ab2dd6c5d9
lingshin <lingshin@youngu.com>
2025-04-29 01:37:28 +0800

    升级了截图工具，同时将壁纸的随机下一张改成了随机列表
    截图工具换用了支持标注的satty
    原先壁纸的随机切换同时容易切换到上一张甚至是目前这一张
    现在几乎避免了，为什么是几乎呢？因为更新列表时是不考虑最后一张的

--- .config/lingshin/scripts/swww/random.sh
+++ .config/lingshin/scripts/swww/random.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 lingshin=~/.config/lingshin
-"$lingshin"/scripts/swww/switch.sh $(find "$lingshin"/wallpaper/random/ -type f | shuf -n 1)
+list_f="$lingshin"/scripts/swww/list
+index_f="$lingshin"/scripts/swww/index
+index=$(cat "$index_f")
+
+wallpaperpaper=$(sed -n "$index"p "$list_f")
+"$lingshin"/scripts/swww/switch.sh "$wallpaperpaper"
+
+if [ "$index" = 1 ]; then
+  list=$(find "$lingshin"/wallpaper/random/ -type f)
+  echo "$list" | shuf >"$list_f"
+  echo "$list" | wc -l >"$index_f"
+else
+  echo $((index - 1)) >"$index_f"
+fi
