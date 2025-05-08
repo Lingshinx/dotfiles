19b91d6c5d47383d82e817b31073e0edea24f28b
lingshin <yokaringx@outlook.com>
2025-05-08 19:53:02 +0800

    分解了kitty配置，添加了kittyscrollback

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -1,11 +1,13 @@
-alias man=tldr
-alias tsdr="\\man"
+alias man='tldr'
+alias tsdr='\\man'
 
-alias transv="trans -verbose -theme random -e google"
+alias plz='sudo'
 
-alias ls="lsd"
-alias la="lsd -a"
-alias tree="lsd --tree --depth=2"
+alias transv='trans -verbose -theme random -e google'
+
+alias ls='lsd'
+alias la='lsd -a'
+alias tree='lsd --tree --depth=2'
 
 alias congif='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias lazygif='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
