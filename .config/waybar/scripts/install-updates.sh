bb2021a814c439b1480b1548b7fb841fde84da00
lingshin <yokaringx@outlook.com>
2025-05-19 22:06:23 +0800

    waybar: replace gum with a img

--- .config/waybar/scripts/install-updates.sh
+++ .config/waybar/scripts/install-updates.sh
@@ -1,13 +1,5 @@
-if gum confirm "DO YOU WANT TO START THE UPDATE NOW?"; then
-  echo
-  echo ":: Update started."
-elif [ $? -eq 130 ]; then
-  exit 130
-else
-  echo
-  echo ":: Update canceled."
-  exit
-fi
+echo
+kitten icat ~/.config/waybar/assets/smaller.png
 
 https_proxy=http://127.0.0.1:7890 \
   http_proxy=http://127.0.0.1:7890 \
