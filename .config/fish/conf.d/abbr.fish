2f7e95d0793fa6351d6bdb6574ffc7488cff65fb
lingshin <yokaringx@outlook.com>
2025-05-30 21:53:32 +0800

    fish: add some abbreviations
    ... and ....
    ll for lsd -l
    tsdr for man
    rm for trash

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,9 +1,16 @@
+abbr -a ... -- ../..
+abbr -a .... -- ../../..
+
 abbr -a man -- tldr
-abbr -a tsdr -- command man
+abbr -a tsdr -- man
+
 abbr -a plz -- sudo
 
 abbr -a transv -- trans -verbose -theme random -e google
 
 abbr -a ls -- lsd
 abbr -a la -- lsd -a
+abbr -a ll -- lsd -l
 abbr -a tree -- lsd --tree --depth=2
+
+abbr -a rm -- trash
