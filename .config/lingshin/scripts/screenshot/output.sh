bf9a8b5d55173ee5b9c003a280dbf6e1fa6420c6
lingshin <yokaringx@outlook.com>
2025-06-11 15:52:12 +0800

    screenshot change dir name Screenshot to Screenshot, which is correct

--- .config/lingshin/scripts/screenshot/output.sh
+++ .config/lingshin/scripts/screenshot/output.sh
@@ -1,3 +1,3 @@
 monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
 
-grim -o "$monitor" -t ppm - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png --copy-command=wl-copy
+grim -o "$monitor" -t ppm - | satty --filename - --fullscreen --output-filename "$HOME/Pictures/Screenshot/satty-$(date '+%Y%m%d-%H:%M:%S').png" --copy-command=wl-copy
--- .config/lingshin/scripts/screenshot/region.sh
+++ .config/lingshin/scripts/screenshot/region.sh
@@ -1,1 +1,1 @@
-grim -g "$(slurp -c \#ffffffff)" -t ppm - | satty --filename - --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png --copy-command=wl-copy
+grim -g "$(slurp -c \#ffffffff)" -t ppm - | satty --filename - --output-filename "$HOME/Pictures/Screenshot/satty-$(date '+%Y%m%d-%H:%M:%S').png" --copy-command=wl-copy
