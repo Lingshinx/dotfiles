50fbbd4f2eef19a4221e46cfd2c81aadba64d03a
lingshin <yokaringx@outlook.com>
2025-06-11 22:27:46 +0800

    wallpaper changes per minute when idle

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+active=$(hyprctl activewindow)
+if test "$active" != "Invalid"; then
+  exit 0
+fi
+
 monitors=$(hyprctl monitors -j | jq -r '.[] | .name')
 echo "$monitors" | while read -r line; do
-  swww img --transition-fps 60 -t grow ~/.config/lingshin/wallpaper/"$line".png -o "$line"
+  random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/random)"
+  swww img "$random" -type wipe --output "$line" --transition-fps 60 --transition-angle 340
 done
