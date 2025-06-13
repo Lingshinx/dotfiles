9904e7544eaae4992e7b30520d4e460a96501f26
lingshin <yokaringx@outlook.com>
2025-06-13 17:48:29 +0800

    swww: init.sh changed

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-active=$(hyprctl activewindow)
-if test "$active" != "Invalid"; then
-  exit 0
+id=$(hyprctl activewindow -j | jq '.workspace.id')
+if test "$id" != null; then
+  active_monitor=$(hyprctl workspaces -j | jq -r ".[] | select(.id == $id) | .monitor")
 fi
 
-monitors=$(hyprctl monitors -j | jq -r '.[] | .name')
+monitors=$(hyprctl monitors -j | jq -r ".[] | select(.name != \"$active_monitor\") | .name")
+
+if test -z "$monitors"; then
+  exit
+fi
+
 echo "$monitors" | while read -r line; do
   random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/random)"
-  swww img "$random" -type wipe --output "$line" --transition-fps 60 --transition-angle 340
+  swww img "$random" --transition-type wipe --outputs "$line" --transition-fps 60 --transition-angle 340
 done
