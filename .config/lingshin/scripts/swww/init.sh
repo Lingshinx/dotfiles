c71142b071c23b19aa9af147de5cf811b754830b
lingshin <yokaringx@outlook.com>
2025-06-16 20:42:10 +0800

    fix(swww): it will change wallpaper even when workspace is focused
    因为现在是 hypridle 在控制切换壁纸了，已经可以保证我在工作时得到专注了

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-id=$(hyprctl activewindow -j | jq '.workspace.id')
-if test "$id" != null; then
-  active_monitor=$(hyprctl workspaces -j | jq -r ".[] | select(.id == $id) | .monitor")
-fi
-
-monitors=$(hyprctl monitors -j | jq -r ".[] | select(.name != \"$active_monitor\") | .name")
-
-if test -z "$monitors"; then
-  exit
-fi
+monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
 echo "$monitors" | while read -r line; do
   random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/random)"
