f31766a81448f3585f3bac93a9ba19822708bd05
lingshin <yokaringx@outlook.com>
2025-06-22 17:35:35 +0800

    fix(fish.env): remove unnecessary env for cargo bin

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,3 +1,3 @@
 set -gx MANPAGER "nvim +Man!"
-set -gx PATH $PATH ~/Desktop/Application/scripts ~/.cargo/bin ~/Desktop/Application/bin
+set -gx PATH $PATH ~/Desktop/Application/scripts ~/Desktop/Application/bin
 set -gx GOPATH $HOME/.go $HOME/Desktop/Workspace/go
