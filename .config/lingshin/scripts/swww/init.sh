eae470ea2a0b99f98d11604e77155f99c573c0f5
lingshin <yokaringx@outlook.com>
2025-07-01 16:06:16 +0800

    refactor(lingshin): add a new monitor
    我现在加了一个竖着的便捷屏

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -2,7 +2,7 @@
 
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
-echo "$monitors" | while read -r line; do
-  random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/random)"
-  swww img "$random" --transition-type wipe --outputs "$line" --transition-fps 60 --transition-angle 340
+echo "$monitors" | while read -r monitor; do
+  random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/"$monitor")"
+  swww img "$random" --transition-type wipe --outputs "$monitor" --transition-fps 60 --transition-angle 340
 done
--- .config/lingshin/scripts/swww/random.sh
+++ .config/lingshin/scripts/swww/random.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 lingshin=~/.config/lingshin
-wallpaper_dir="$lingshin/wallpaper/random/"
+monitor="$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')"
+wallpaper_dir="$lingshin/wallpaper/$monitor/"
 
-$lingshin/scripts/swww/switch.sh "$($lingshin/scripts/tools/random.fish "$wallpaper_dir")"
+$lingshin/scripts/swww/switch.sh "$($lingshin/scripts/tools/random.fish "$wallpaper_dir")" --outputs $monitor
