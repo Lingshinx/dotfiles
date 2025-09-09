4056480bf1a209bf9f90ba1fc3f3d4c309e0ce5c
lingshin <yokaringx@outlook.com>
2025-09-09 19:24:51 +0800

    fix(swww,qq): compitable with niri

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/fish
 
-monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
+set monitors (niri msg --json focused-output | jq -r .name)
 
-echo "$monitors" | while read -r monitor; do
-  random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/"$monitor")"
+for monitor in $monitors
+  set random (randir ~/.config/lingshin/wallpaper/$monitor)
   swww img "$random" --transition-type wipe --outputs "$monitor" --transition-fps 60 --transition-angle 340
-done
+end
