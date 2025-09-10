7b787d1971f1a88b8954a552cf4ab970e744b816
lingshin <yokaringx@outlook.com>
2025-09-10 15:11:40 +0800

    fix(swww.init): correct cmd

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -2,7 +2,7 @@
 
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
-echo "$monitors" | while read -r monitor; do
-  random="$(~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/wallpaper/"$monitor")"
+for monitor in $monitors
+  set random (ramdir ~/.config/lingshin/wallpaper/$monitor)
   swww img "$random" --transition-type wipe --outputs "$monitor" --transition-fps 60 --transition-angle 340
 done
