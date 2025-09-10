d65be4c5864a93e3f602c06a14b3787e6af470bd
lingshin <yokaringx@outlook.com>
2025-09-10 15:11:40 +0800

    fix(swww.init): correct cmd

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -3,6 +3,6 @@
 set monitors (niri msg --json focused-output | jq -r .name)
 
 for monitor in $monitors
-  set random (randir ~/.config/lingshin/wallpaper/$monitor)
+  set random (ramdir ~/.config/lingshin/wallpaper/$monitor)
   swww img "$random" --transition-type wipe --outputs "$monitor" --transition-fps 60 --transition-angle 340
 end
