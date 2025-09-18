b825f189613568761aa559cfa32820637a3759a6
lingshin <yokaringx@outlook.com>
2025-09-18 18:12:16 +0800

    fix(lingshin.scripts.screenshot): compatible with niri

--- .config/lingshin/scripts/screenshot/output.sh
+++ .config/lingshin/scripts/screenshot/output.sh
@@ -1,3 +1,5 @@
-monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
+#!/bin/fish
+
+set monitor (niri msg --json focused-output | jq -r .name)
 
 grim -o "$monitor" -t ppm - | satty --filename - --fullscreen --output-filename "$HOME/Pictures/Screenshot/satty-$(date '+%Y%m%d-%H:%M:%S').png" --copy-command=wl-copy
