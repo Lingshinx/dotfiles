6a64d21fb8f54b13e284beff0986f14c0a73de4b
lingshin <yokaringx@outlook.com>
2025-09-19 17:47:53 +0800

    fix(scripts.swww): use fish instead and support rotated output

--- .config/lingshin/scripts/swww/cursor.js
+++ .config/lingshin/scripts/swww/cursor.js
@@ -1,18 +1,22 @@
 #!/bin/node
 
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
-const pos = execSync('hyprctl cursorpos').toString().split(',').map(x => parseInt(x))
-const monitor = JSON.parse(
-  execSync('hyprctl monitors -j').toString()
-).find(monitor => monitor.focused === true);
+const pos = execSync("hyprctl cursorpos")
+  .toString()
+  .split(",")
+  .map((x) => parseInt(x));
+const monitor = JSON.parse(execSync("hyprctl monitors -j").toString()).find(
+  (monitor) => monitor.focused === true,
+);
 
-const { height, x, y, scale } = monitor
+const { width, height, x, y, scale, transform } = monitor;
 
 const pos_x_raw = pos[0] - x;
 const pos_y_raw = pos[1] - y;
-
-const pos_x = Math.round(pos_x_raw * scale)
-const pos_y = Math.round(height - pos_y_raw * scale)
+const even = (x) => x % 2 == 0;
+const real_height = even(transform) ? height : width;
+const pos_x = Math.round(pos_x_raw * scale);
+const pos_y = Math.round(real_height - pos_y_raw * scale);
 
 console.log(`-o ${monitor.name} --transition-pos ${pos_x},${pos_y}`);
--- .config/lingshin/scripts/swww/random.sh
+++ .config/lingshin/scripts/swww/random.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/fish
 
-lingshin=~/.config/lingshin
-monitor="$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')"
-wallpaper_dir="$lingshin/wallpaper/$monitor/"
+set lingshin ~/.config/lingshin
+set monitor (hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+set wallpaper_dir $lingshin/wallpaper/$monitor
 
-$lingshin/scripts/swww/switch.sh "$($lingshin/scripts/tools/random.fish "$wallpaper_dir")" --outputs $monitor
+$lingshin/scripts/swww/switch.sh (randir $wallpaper_dir) --outputs $monitor
