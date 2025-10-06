f45a21022eeab7465ee625c7129a843aa99315ea
lingshin <yokaringx@outlook.com>
2025-10-06 12:05:05 +0800

    fix(lingshin.scripts.swww)

--- .config/lingshin/scripts/swww/init.sh
+++ .config/lingshin/scripts/swww/init.sh
@@ -1,6 +1,6 @@
 #!/bin/fish
 
-for monitor in (niri msg --json focused-output | jq -r .name)
+for monitor in (niri msg --json outputs | jq -r '.[] | .name')
   set random (randir ~/.config/lingshin/wallpaper/$monitor)
   swww img "$random" --transition-type wipe --outputs "$monitor" --transition-fps 60 --transition-angle 340
 end
--- .config/lingshin/scripts/swww/switch.sh
+++ .config/lingshin/scripts/swww/switch.sh
@@ -1,3 +1,3 @@
 #!/bin/fish
 
-swww img --transition-fps 60 -t grow $argv[1] -o (niri msg --json focused-output | jq -r .name)
+swww img --transition-fps 60 -t grow "$argv" -o (niri msg --json focused-output | jq -r .name)
