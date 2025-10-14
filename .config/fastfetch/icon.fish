5baecb34b774ce1b1dc3e8c1215b960703e84420
lingshin <yokaringx@outlook.com>
2025-10-14 14:47:44 +0800

    fix(fastfetch.icon.sh): use randir instead

--- .config/fastfetch/icon.fish
+++ .config/fastfetch/icon.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 if string match '*kitty' $TERM --quiet
-    ~/.config/lingshin/scripts/tools/random.fish ~/.config/lingshin/fastfetch-icons
+    randir ~/.config/lingshin/fastfetch-icons
 else
     echo hello
 end
