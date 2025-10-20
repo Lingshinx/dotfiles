4188f33934b686838cc8ba5d91d7208cae65fdc3
lingshin <yokaringx@outlook.com>
2025-10-20 22:46:38 +0800

    fix(lingshin.scripts.qrcode): use xdg-open instead of hard coded firefox

--- .config/lingshin/scripts/screenshot/qrcode.sh
+++ .config/lingshin/scripts/screenshot/qrcode.sh
@@ -4,7 +4,7 @@ grim -g (slurp) (push)
 set output (string replace --regex ^QR-Code: '' (zbarimg -q (top)))
 echo $output
 if string match --quiet 'http*' -- "$output"
-  firefox $output
+  xdg-open $output
 else if test -n "$output"
   notify-send "QRCode copied" "$output"
   wl-copy -- $output
