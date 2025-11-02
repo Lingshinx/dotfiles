f488c1ba3e072e4663b40062819cc34540d3f4bd
lingshin <yokaringx@outlook.com>
2025-11-02 12:02:50 +0800

    fix(rofi.script.game): all game pictures available on my device now
    顺便还使用了我后来新学会的 fish 用法，更多地使用了 choose

--- .config/lingshin/scripts/rofi/gamelauncher.fish
+++ .config/lingshin/scripts/rofi/gamelauncher.fish
@@ -1,48 +1,43 @@
 #!/bin/fish
 
-set libraryThumbName "library_600x900.jpg"
+set libraryThumbName 'library_(600x900.*|capsule)\.jpg'
 set launcherConfig ~/.config/rofi/game/gamelauncher_4.rasi
 set steamLib ~/.local/share/Steam/config/libraryfolders.vdf
 set steamThumb ~/.local/share/Steam/appcache/librarycache
 
 set manifestList (
-  rg path $steamLib | awk -F '"' '{print $4}' | while read sp
-     fd 'appmanifest_.*\.acf' $sp --type file 2>/dev/null
+  rg path $steamLib | choose -f \" 3 | while read sp
+     fd 'appmanifest_.*\.acf$' $sp --type file 2>/dev/null
   end
 )
 
-if test -z "$manifestList"
-    notify-send -a HyDE "Cannot Fetch Steam Games!" && exit 1
-end
+test -z "$manifestList"
+and notify-send -a HyDE "Cannot Fetch Steam Games!" 
+and exit 1
 
 set games (
   for manifest in $manifestList
-    set appid (rg appid $manifest | cut -d '"' -f 4)
-    set name (rg name $manifest | cut -d '"' -f 4)
+    set appid (rg '"appid"' $manifest | choose -f \" 3)
+    set name (rg name $manifest | choose -f \" 3)
 
-    if ! string match --regex --quiet '.*(Proton|Steam).*' $name
-       echo "$name|$appid"
-    end
+    string match --regex 'Proton|Steam' --quiet $name
+    or echo "$name|$appid"
   end
 )
 
-set choosenGame (
-  for game in $games
-      set game (string split '|' $game)
-      set name $game[1]
-      set appid $game[2]
+for game in $games
+    string split '|' $game | read --line name appid
 
-      set image (fd $libraryThumbName --type file $steamThumb/$appid | head -1)
-      printf  "%s\x00icon\x1f$image\n" $name
-  end | rofi -dmenu \
-      -config $launcherConfig
-)
+    set image (fd $libraryThumbName --type file $steamThumb/$appid | head -1)
+    printf "%s\x00icon\x1f%s\n" $name $image
+end | rofi -dmenu \
+    -config $launcherConfig | read choosenGame
 
-if test -n "$choosenGame"
-    set launchId (printf %s\n $games | rg $choosenGame | cut -d '|' -f 2)
-    echo $launchId
-    set headerImage (fd ".*header.jpg" "$steamThumb/$launchId/" --type file)
+test -z "$choosenGame"
+and exit 1
 
-    steam -applaunch "$launchId [gamemoderun %command%]" -silent &
-    notify-send -a Steam -i "$headerImage" "Launching $choosenGame..."
-end
+set launchId (printf %s\n $games | rg $choosenGame | choose -f '|' 1)
+set headerImage (fd ".*header.jpg" --type file $steamThumb/$launchId)
+
+notify-send -a Steam -i "$headerImage" "Launching $choosenGame..."
+steam -applaunch "$launchId [gamemoderun %command%]" -silent
